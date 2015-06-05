37f0d378c63c3a1daae0690eb2f1c6fc8bbd00c1
Cesar Velandia <cesar@velandia.co>
2015-06-05 02:27:40 +0000

    session 3

---
+++ for_loops_dict.py
@@ -0,0 +1,4 @@
+food = {'dessert':  'ice cream', 'meal':  'hot dog', 'breakfast': 'pancake'}
+
+for key in food:
+	print "key is "+ key + " and value is "+ food[key]
---
+++ while_loops.py
@@ -0,0 +1,7 @@
+food = ['ice cream', 'hot dog', 'pancake']
+
+i = 0
+
+while (i < len(food)):
+	print food[i]
+	i = i + 1
